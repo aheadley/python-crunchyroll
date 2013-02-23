26cef8a402a0b71ba31491f567fee3818ed0a84f
aheadley <aheadley@waysaboutstuff.com>
2013-02-23 14:43:56 -0500

    stub out subtitle formatters

--- crunchyroll/subtitles.py
+++ crunchyroll/subtitles.py
@@ -3,14 +3,15 @@
 import math
 import hashlib
 import zlib
+from xml.dom.minidom import parseString as minidom_parseString
 
 try:
     from crypto.cipher.aes_cbc import AES_CBC
     from crypto.cipher.base import noPadding
-    _crypto_is_old = True
+    _LEGACY_CRYPTO = True
 except ImportError:
     from Crypto.Cipher.AES import MODE_CBC, AESCipher
-    _crypto_is_old = False
+    _LEGACY_CRYPTO = False
 
 class SubtitleDecrypter(object):
     """Decrypt Crunchyroll's encrypted subtitle data
@@ -38,7 +39,8 @@ class SubtitleDecrypter(object):
         @param str encrypted_data
         @return str
         """
-        encryption_key = self._build_encryption_key(subtitle_id)
+
+        encryption_key = self._build_encryption_key(int(subtitle_id))
         decryption_func = self._get_decryption_func(encryption_key, iv.decode('base64'))
         return zlib.decompress(decryption_func(encrypted_data.decode('base64')))
 
@@ -46,7 +48,7 @@ class SubtitleDecrypter(object):
         """Get a function to do encryption based on the version of the pycrypto
         module being used
         """
-        if _crypto_is_old:
+        if _LEGACY_CRYPTO:
             def decrypt_func(data):
                 cipher = AES_CBC(encryption_key, padding=noPadding(),
                     keySize=len(encryption_key))
@@ -60,13 +62,13 @@ class SubtitleDecrypter(object):
     def _build_encryption_key(self, subtitle_id, key_size=ENCRYPTION_KEY_SIZE):
         """Generate the encryption key for a given media item
 
-        @param int subtitle_id
-        @param int key_size
-        @return str
-
         Encryption key is basically just
         sha1(<magic value based on subtitle_id> + '"#$&).6CXzPHw=2N_+isZK') then
         padded with 0s to 32 chars
+
+        @param int subtitle_id
+        @param int key_size
+        @return str
         """
 
         # generate a 160-bit SHA1 hash
@@ -79,10 +81,10 @@ class SubtitleDecrypter(object):
     def _build_hash_magic(self, subtitle_id):
         """Build the other half of the encryption key hash
 
+        I have no idea what is going on here
+
         @param int subtitle_id
         @return str
-
-        I have no idea what is going on here
         """
 
         media_magic = self.HASH_MAGIC_CONST ^ subtitle_id
@@ -93,15 +95,15 @@ class SubtitleDecrypter(object):
             mod_value=HASH_SECRET_MOD_CONST):
         """Build a seed for the hash based on the Fibonacci sequence
 
-        @param tuple|list seq_seed
-        @param int seq_len
-        @param int mod_value
-        @return str
-
         Take first `seq_len` + len(`seq_seed`) characters of Fibonacci
         sequence, starting with `seq_seed`, and applying e % `mod_value` +
         `HASH_SECRET_CHAR_OFFSET` to the resulting sequence, then return as
         a string
+
+        @param tuple|list seq_seed
+        @param int seq_len
+        @param int mod_value
+        @return str
         """
 
         # make sure we use a list, tuples are immutable
@@ -112,3 +114,64 @@ class SubtitleDecrypter(object):
             lambda c: chr(c % mod_value + self.HASH_SECRET_CHAR_OFFSET),
             fbn_seq[2:])
         return ''.join(hash_secret)
+
+class SubtitleFormatter(object):
+    """Base subtitle formatter class
+    """
+
+    def format(self, sub_xml_text):
+        """Turn a string containing the subs xml document into the formatted
+        subtitle string
+
+        @param str sub_xml_text
+        @return str
+        """
+
+        xml_doc = minidom_parseString(sub_xml_text)
+        return self._format(xml_doc)
+
+    def _format(self, sub_xml_doc):
+        """Do the actual formatting on the parsed xml document, should be
+        overridden by subclasses
+
+        @param xml.dom.Document sub_xml_doc
+        @return str
+        """
+        raise NotImplemented
+
+class ASSFormatter(SubtitleFormatter):
+    """Subtitle formatter for ASS format
+    """
+
+    def _format(self, sub_xml_doc):
+        pass
+
+    def _format_style(self, style_element):
+        pass
+
+    def _format_event(self, event_element):
+        pass
+
+class SRTFormatter(SubtitleFormatter):
+    """Subtitle formatter for SRT (unstyled) format
+    """
+
+    def _format(self, sub_xml_doc):
+        # TODO: finish this
+        return '\n\n'.join(self._format_event(idx, event) \
+            for idx, event in enumerate(sorted(events)))
+
+    def _format_event(self, index, event):
+        pass
+
+    def _format_timestamp(self, timestamp):
+        """Format timestamp to what SRT wants
+
+        Turns a string like 0:00:04.17 -> 00:00:04,170
+
+        @param str timestamp
+        @return str
+        """
+        # TODO: milliseconds are wrong, should be left-justified
+        return '{0:02d}:{1:02d}:{2:02d},{3:03d}'.format(
+            *map(int, timestamp.replace('.', ':').split(':')))
