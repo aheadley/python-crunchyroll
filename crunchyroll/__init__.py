e922cf49e834ece20255fef9543639c2fee38719
aheadley <aheadley@nexcess.net>
2013-02-27 14:38:15 -0500

    implement setup.py

--- crunchyroll/__init__.py
+++ crunchyroll/__init__.py
@@ -17,6 +17,32 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-if __name__ == '__main__':
-    # want to do stuff here eventually
-    pass
+"""
+Crunchyroll Library
+-------------------
+
+A library to interface with Crunchyroll's various APIs and utilites to work
+with the returned data
+
+:copyright: (c) 2013 by Alex Headley
+:license: GNU General Public License v2+, see LICENCE for more details
+"""
+
+__title__           = 'crunchyroll'
+__version__         = '0.0.1'
+__author__          = 'Alex Headley'
+__author_email__    = 'aheadley@waysaboutstuff.com'
+__license__         = 'GNU General Public License v2+'
+__copyright__       = 'Copyright 2013 Alex Headley'
+__url__             = 'https://github.com/aheadley/python-crunchyroll'
+
+# set default logging handler
+import logging
+try:
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
--- setup.py
+++ setup.py
@@ -16,3 +16,28 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import crunchyroll
+
+packages = [
+    'crunchyroll',
+    'crunchyroll.apis',
+]
+
+setup(
+    name='crunchyroll',
+    version=crunchyroll.__version__,
+    description='Library to interface with Crunchyroll\'s APIs',
+    long_description=open('README.md').read(),
+    author=crunchyroll.__author__,
+    author_email=crunchyroll.__author_email__,
+    url=crunchyroll.__url__,
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    license=open('LICENSE').read()
+)
